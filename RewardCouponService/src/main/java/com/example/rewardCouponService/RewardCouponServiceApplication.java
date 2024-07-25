c380ca60b182c6fbfe1596682ebcc7128eece41e
kartheekcvs <kartheek.cvs@gmail.com>
2024-07-25 11:28:35 +0530

    Initial commit

---
+++ RewardCouponService/src/main/java/com/example/rewardCouponService/RewardCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rewardCouponService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardCouponServiceApplication.class, args);
+	}
+
+}
---
+++ RewardCouponService/src/test/java/com/example/rewardCouponService/RewardCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rewardCouponService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
